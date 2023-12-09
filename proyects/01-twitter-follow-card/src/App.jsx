495ed1510f74af4014df9b4b1c7b08a5f1b87ba0
AdrianGerman <adrian.german1019@gmail.com>
2023-12-09 12:13:43 -0600

    styles with container

--- proyects/01-twitter-follow-card/src/App.jsx
+++ proyects/01-twitter-follow-card/src/App.jsx
@@ -3,14 +3,14 @@ import "./App.css";
 
 function App() {
   return (
-    <>
+    <section className="App">
       <TwitterFollowCard
         userName={"AdrianGermn1"}
         name={"Adrian German Becerra"}
       />
       <TwitterFollowCard userName={"nilojeda"} name={"Nil Ojeda"} />
       <TwitterFollowCard userName={"Andronews_net"} name={"Juan Hernandez"} />
-    </>
+    </section>
   );
 }
 
