89cdb10998cacf951ce2686aad69427795873337
AdrianGerman <adrian.german1019@gmail.com>
2024-01-02 10:09:32 -0600

    pass functions as props

--- proyects/01-twitter-follow-card/src/App.jsx
+++ proyects/01-twitter-follow-card/src/App.jsx
@@ -2,22 +2,32 @@ import { TwitterFollowCard } from "./TwitterFollowCard";
 import "./App.css";
 
 function App() {
+  const format = (userName) => `@${userName}`;
   return (
     <section className="App">
       <TwitterFollowCard
-        isFollowing={true}
+        formatUserName={format}
+        isFollowing
         userName={"AdrianGermn1"}
         name={"Adrian German"}
       />
       <TwitterFollowCard
+        formatUserName={format}
         isFollowing={false}
         userName={"nilojeda"}
         name={"Nil Ojeda"}
       />
       <TwitterFollowCard
+        formatUserName={format}
         isFollowing
         userName={"Andronews_net"}
         name={"Juan Hernandez"}
+      />
+      <TwitterFollowCard
+        formatUserName={format}
+        isFollowing
+        userName={"benistofeles"}
+        name={"Benito Taibo"}
       />
     </section>
   );
--- proyects/01-twitter-follow-card/src/TwitterFollowCard.jsx
+++ proyects/01-twitter-follow-card/src/TwitterFollowCard.jsx
@@ -1,21 +1,24 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
-export function TwitterFollowCard({ userName, name, isFollowing }) {
-  const addAt = (userName) => `@${userName}`;
-
+export function TwitterFollowCard({
+  formatUserName,
+  userName,
+  name,
+  isFollowing,
+}) {
   return (
     <>
       <article className="tw-followCard">
         <header className="tw-followCard-header">
           <img
             className="tw-followCard-avatar"
-            src={`https://unavatar.io/${addAt(userName)}`}
+            src={`https://unavatar.io/${formatUserName(userName)}`}
             alt="avatar persona 1"
           />
           <div className="tw-followCard-info">
             <strong>{name}</strong>
             <span className="tw-followCard-infoUsername">
-              {addAt(userName)}
+              {formatUserName(userName)}
             </span>
           </div>
         </header>
