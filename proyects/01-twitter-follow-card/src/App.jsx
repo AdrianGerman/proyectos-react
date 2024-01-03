416cc4e0c20353db2a8b495ff8c8afc31c5cad6d
AdrianGerman <adrian.german1019@gmail.com>
2024-01-03 11:02:14 -0600

    list and finil commit

--- proyects/01-twitter-follow-card/src/App.jsx
+++ proyects/01-twitter-follow-card/src/App.jsx
@@ -1,22 +1,41 @@
 import { TwitterFollowCard } from "./TwitterFollowCard.jsx";
 import "./App.css";
 
+const users = [
+  {
+    userName: "AdrianGermn1",
+    name: "Adrian German",
+    isFollowing: true,
+  },
+  {
+    userName: "nilojeda",
+    name: "Nil Ojete",
+    isFollowing: true,
+  },
+  {
+    userName: "TeamCherryGames",
+    name: "Gregorio Dominic Rupertino Rodriguez",
+    isFollowing: false,
+  },
+  {
+    userName: "benistofeles",
+    name: "Benito Taibo",
+    isFollowing: false,
+  },
+];
+
 function App() {
   return (
     <section className="App">
-      <TwitterFollowCard userName={"AdrianGermn1"} initialIsFollowing>
-        Adrian German
-      </TwitterFollowCard>
-
-      <TwitterFollowCard userName={"nilojeda"}>Nil Ojeda</TwitterFollowCard>
-
-      <TwitterFollowCard userName={"Andronews_net"} initialIsFollowing>
-        Juan Hernandez
-      </TwitterFollowCard>
-
-      <TwitterFollowCard userName={"benistofeles"}>
-        Benito Taibo
-      </TwitterFollowCard>
+      {users.map(({ userName, name, isFollowing }) => (
+        <TwitterFollowCard
+          key={userName}
+          userName={userName}
+          initialIsFollowing={isFollowing}
+        >
+          {name}
+        </TwitterFollowCard>
+      ))}
     </section>
   );
 }
