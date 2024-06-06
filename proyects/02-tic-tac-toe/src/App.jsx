4b2fc8e93545bb233a5c8ee7c271ecc9c72f1aca
AdrianGerman <adrian.german1019@gmail.com>
2024-06-06 09:52:05 -0600

    initializing the state

--- proyects/02-tic-tac-toe/src/App.jsx
+++ proyects/02-tic-tac-toe/src/App.jsx
@@ -1,15 +1,17 @@
+import { useState } from "react";
+
 const TURNS = {
   X: "x",
   O: "o"
 };
-
-const board = Array(9).fill(null);
 
 const Square = ({ children, updateBoard, index }) => {
   return <div className="square">{children}</div>;
 };
 
 function App() {
+  const [board, setBoard] = useState(Array(9).fill(null));
+
   return (
     <main className="board">
       <h1>Tic tac toe </h1>
