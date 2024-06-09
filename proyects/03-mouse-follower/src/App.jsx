30a8318af31abe99abb69bc9a867a2fae6618235
AdrianGerman <adrian.german1019@gmail.com>
2024-06-09 09:18:42 -0600

    stop event

--- proyects/03-mouse-follower/src/App.jsx
+++ proyects/03-mouse-follower/src/App.jsx
@@ -15,6 +15,10 @@ function App() {
     if (enabled) {
       window.addEventListener("pointermove", handleMove);
     }
+
+    return () => {
+      window.removeEventListener("pointermove", handleMove);
+    };
   }, [enabled]);
   return (
     <>
