897caca1b7473c0f031e8aa556512a5ce18d2cd2
AdrianGerman <adrian.german1019@gmail.com>
2024-06-10 12:35:15 -0600

    useEffect and cat endpoint random fact

--- projects/04-react-prueba-tecnica/src/App.jsx
+++ projects/04-react-prueba-tecnica/src/App.jsx
@@ -1,12 +1,22 @@
 import { useState } from "react";
 import "./App.css";
+import { useEffect } from "react";
+
+const CAT_ENDPOINT_RANDOM_FACT = "https://catfact.ninja/fact";
 
 function App() {
   const [fact, setFact] = useState("texto texto texto texto texto texto ");
+
+  useEffect(() => {
+    fetch(CAT_ENDPOINT_RANDOM_FACT)
+      .then((res) => res.json())
+      .then((data) => setFact(data.fact));
+  }, []);
+
   return (
     <main>
-      <h1>Hello, world!</h1>
-      <p>{fact}</p>
+      <h1>App de gatitos</h1>
+      {fact && <p>{fact}</p>}
     </main>
   );
 }
