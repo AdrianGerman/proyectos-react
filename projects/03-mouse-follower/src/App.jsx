8a135dc411b2cec9f196b9e4b310a4d6425b698a
AdrianGerman <adrian.german1019@gmail.com>
2024-06-10 09:15:26 -0600

    new useEffect

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -3,6 +3,8 @@ import { useEffect, useState } from "react";
 const FollowMouse = () => {
   const [enabled, setEnabled] = useState(false);
   const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  // pointer move
   useEffect(() => {
     console.log("efecto", { enabled });
 
@@ -19,6 +21,14 @@ const FollowMouse = () => {
     return () => {
       console.log("cleanup");
       window.removeEventListener("pointermove", handleMove);
+    };
+  }, [enabled]);
+
+  // change body className
+  useEffect(() => {
+    document.body.classList.toggle("no-cursor", enabled);
+    return () => {
+      document.body.classList.remove("no-cursor");
     };
   }, [enabled]);
 
