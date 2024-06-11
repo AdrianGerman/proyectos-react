05509932b554a23f81677a1f8557ab1e4c067717
AdrianGerman <adrian.german1019@gmail.com>
2024-06-11 09:42:57 -0600

    shows an image of a cat with the recovered words

--- projects/04-react-prueba-tecnica/src/App.jsx
+++ projects/04-react-prueba-tecnica/src/App.jsx
@@ -5,18 +5,36 @@ import { useEffect } from "react";
 const CAT_ENDPOINT_RANDOM_FACT = "https://catfact.ninja/fact";
 
 function App() {
-  const [fact, setFact] = useState("texto texto texto texto texto texto ");
+  const [fact, setFact] = useState();
+  const [imageUrl, setImageUrl] = useState();
 
   useEffect(() => {
     fetch(CAT_ENDPOINT_RANDOM_FACT)
       .then((res) => res.json())
-      .then((data) => setFact(data.fact));
+      .then((data) => {
+        const { fact } = data;
+        setFact(fact);
+
+        const threeFirstWords = fact.split(" ", 3).join(" ");
+        // slice(0, 3).join("");
+        console.log(threeFirstWords);
+
+        fetch(`https://cataas.com/cat/says/${threeFirstWords}?size=50&color=red&json=true`)
+          .then((res) => res.json())
+          .then((response) => {
+            const { url } = response;
+            setImageUrl(`https://cataas.com${url}`);
+          });
+      });
   }, []);
 
   return (
     <main>
       <h1>App de gatitos</h1>
       {fact && <p>{fact}</p>}
+      {imageUrl && (
+        <img src={imageUrl} alt={`Image extracted using the first three words for ${fact}`} />
+      )}
     </main>
   );
 }
