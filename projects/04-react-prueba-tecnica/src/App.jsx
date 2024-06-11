72ef67c6b12719ccb68132bbc98a1d3b7b814637
AdrianGerman <adrian.german1019@gmail.com>
2024-06-11 10:40:03 -0600

    separation of effects

--- projects/04-react-prueba-tecnica/src/App.jsx
+++ projects/04-react-prueba-tecnica/src/App.jsx
@@ -7,28 +7,33 @@ function App() {
   const [fact, setFact] = useState();
   const [imageUrl, setImageUrl] = useState();
 
+  // recuperar la cita al cargar la imagen
   useEffect(() => {
     fetch(CAT_ENDPOINT_RANDOM_FACT)
       .then((res) => res.json())
       .then((data) => {
         const { fact } = data;
         setFact(fact);
-
-        const threeFirstWords = fact.split(" ", 3).join(" ");
-        // split(" ")[0]
-        // slice(0, 3).join("");
-        console.log(threeFirstWords);
-
-        fetch(`https://cataas.com/cat/says/${threeFirstWords}?size=50&color=red&json=true`)
-          .then((res) => res.json())
-          .then((response) => {
-            const { _id } = response;
-            const url = `/cat/${_id}/says/${threeFirstWords}`;
-            setImageUrl(url);
-          });
-        // setImageUrl(`https://cataas.com/cat/says/${threeFirstWords}`);
       });
   }, []);
+
+  // recuperar la imagen cada vez que tenemos una cita nueva
+  useEffect(() => {
+    if (!fact) return;
+    const threeFirstWords = fact.split(" ", 3).join(" ");
+    // split(" ")[0]
+    // slice(0, 3).join("");
+    console.log(threeFirstWords);
+
+    fetch(`https://cataas.com/cat/says/${threeFirstWords}?size=50&color=red&json=true`)
+      .then((res) => res.json())
+      .then((response) => {
+        const { _id } = response;
+        const url = `/cat/${_id}/says/${threeFirstWords}`;
+        setImageUrl(url);
+      });
+    // setImageUrl(`https://cataas.com/cat/says/${threeFirstWords}`);
+  }, [fact]);
 
   return (
     <main>
