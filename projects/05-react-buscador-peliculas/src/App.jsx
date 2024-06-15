2349b07ea993e40aac2c7bead2314bb4718eee2e
AdrianGerman <adrian.german1019@gmail.com>
2024-06-15 09:12:46 -0600

    component Movies

--- projects/05-react-buscador-peliculas/src/App.jsx
+++ projects/05-react-buscador-peliculas/src/App.jsx
@@ -1,28 +1,10 @@
 import "./App.css";
+import { Movies } from "./components/Movies";
 import responseMovies from "./mocks/with-results.json";
 // import withoutResults from "./mocks/no-results.json";
 
 function App() {
   const movies = responseMovies.Search;
-  const hasMovies = movies?.length > 0;
-
-  const renderMovies = () => {
-    return (
-      <ul>
-        {movies.map((movie) => (
-          <li key={movie.imdbID}>
-            <h3>{movie.Title}</h3>
-            <p>{movie.Year}</p>
-            <img src={movie.Poster} alt={movie.Title} />
-          </li>
-        ))}
-      </ul>
-    );
-  };
-
-  const renderNoResult = () => {
-    return <p>No se encontraron resultados</p>;
-  };
 
   return (
     <>
@@ -35,7 +17,9 @@ function App() {
           </form>
         </header>
 
-        <main>{hasMovies ? renderMovies() : renderNoResult()}</main>
+        <main>
+          <Movies movies={movies} />
+        </main>
       </div>
     </>
   );
