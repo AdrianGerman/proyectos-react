8f54cd0de470e43cdbad32e6faafef4c10e76cae
AdrianGerman <adrian.german1019@gmail.com>
2024-06-15 08:38:56 -0600

    renderMovies and renderNoResult

--- projects/05-react-buscador-peliculas/src/App.jsx
+++ projects/05-react-buscador-peliculas/src/App.jsx
@@ -1,10 +1,29 @@
 import "./App.css";
 import responseMovies from "./mocks/with-results.json";
-import withoutResults from "./mocks/no-results.json";
+// import withoutResults from "./mocks/no-results.json";
 
 function App() {
   const movies = responseMovies.Search;
   const hasMovies = movies?.length > 0;
+
+  const renderMovies = () => {
+    return (
+      <ul>
+        {movies.map((movie) => (
+          <li key={movie.imdbID}>
+            <h3>{movie.Title}</h3>
+            <p>{movie.Year}</p>
+            <img src={movie.Poster} alt={movie.Title} />
+          </li>
+        ))}
+      </ul>
+    );
+  };
+
+  const renderNoResult = () => {
+    return <p>No se encontraron resultados</p>;
+  };
+
   return (
     <>
       <div className="page">
@@ -16,21 +35,7 @@ function App() {
           </form>
         </header>
 
-        <main>
-          {hasMovies ? (
-            <ul>
-              {movies.map((movie) => (
-                <li key={movie.imdbID}>
-                  <h3>{movie.Title}</h3>
-                  <p>{movie.Year}</p>
-                  <img src={movie.Poster} alt={movie.Title} />
-                </li>
-              ))}
-            </ul>
-          ) : (
-            <p>No se encontraron resultados</p>
-          )}
-        </main>
+        <main>{hasMovies ? renderMovies() : renderNoResult()}</main>
       </div>
     </>
   );
