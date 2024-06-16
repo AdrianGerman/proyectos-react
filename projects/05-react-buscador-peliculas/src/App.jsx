50768eb81cc7566a3676ea21df1b708f80f80e73
AdrianGerman <adrian.german1019@gmail.com>
2024-06-16 07:42:06 -0600

    useEffect

--- projects/05-react-buscador-peliculas/src/App.jsx
+++ projects/05-react-buscador-peliculas/src/App.jsx
@@ -1,11 +1,12 @@
 import { Movies } from "./components/Movies";
 import { useMovies } from "./hooks/useMovies";
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const { movies } = useMovies();
   const [query, setQuery] = useState("");
+  const [error, setError] = useState("");
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -15,6 +16,22 @@ function App() {
   const handleChange = (event) => {
     setQuery(event.target.value);
   };
+
+  useEffect(() => {
+    if (query == "") {
+      setError("No se puede buscar una película vacía");
+      return;
+    }
+    if (query.match(/^\d+$/)) {
+      setError("No se puede buscar una película solo con el número");
+      return;
+    }
+    if (query.length < 3) {
+      setError("La búsqueda debe tener al menos 3 caracteres");
+      return;
+    }
+    setError(null);
+  }, [query]);
 
   return (
     <>
@@ -31,6 +48,7 @@ function App() {
             />
             <button type="submit">Search</button>
           </form>
+          {error && <p style={{ color: "red" }}>{error}</p>}
         </header>
 
         <main>
