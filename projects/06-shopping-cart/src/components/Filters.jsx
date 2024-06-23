5381f44a8fc944cafbee1d76390fc5963a297f52
AdrianGerman <adrian.german1019@gmail.com>
2024-06-23 07:33:31 -0600

    range filter improvements

--- projects/06-shopping-cart/src/components/Filters.jsx
+++ projects/06-shopping-cart/src/components/Filters.jsx
@@ -1,11 +1,19 @@
+import { useState } from "react";
 import "./Filters.css";
 
 export function Filters() {
+  const [minPrice, setMinPrice] = useState(0);
+
+  const handleChangeMinPrice = (event) => {
+    setMinPrice(event.target.value);
+  };
+
   return (
     <section className="filters">
       <div>
         <label htmlFor="price">Price</label>
-        <input type="range" id="price" min="0" max="1000" />
+        <input type="range" id="price" min="0" max="1000" onChange={handleChangeMinPrice} />
+        <span>${minPrice}</span>
       </div>
 
       <div>
