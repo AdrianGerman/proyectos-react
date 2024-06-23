8772b588073099d8c4763255d48811814e8bab13
AdrianGerman <adrian.german1019@gmail.com>
2024-06-23 07:27:08 -0600

    display filters visually

--- projects/06-shopping-cart/src/App.jsx
+++ projects/06-shopping-cart/src/App.jsx
@@ -1,6 +1,7 @@
 import { products as initialProducts } from "./mocks/products.json";
 import { Products } from "./components/Products";
 import { useState } from "react";
+import { Header } from "./components/Header";
 
 function App() {
   const [products] = useState(initialProducts);
@@ -20,6 +21,7 @@ function App() {
   return (
     <>
       <h1>Shopping cart 🛒</h1>
+      <Header />
       <Products products={filteredProducts} />
     </>
   );
---
+++ projects/06-shopping-cart/src/components/Filters.jsx
@@ -0,0 +1,22 @@
+import "./Filters.css";
+
+export function Filters() {
+  return (
+    <section className="filters">
+      <div>
+        <label htmlFor="price">Price</label>
+        <input type="range" id="price" min="0" max="1000" />
+      </div>
+
+      <div>
+        <label htmlFor="category">Categoría</label>
+        <select id="category">
+          <option value="all">Todas</option>
+          <option value="laptops">Portátiles</option>
+          <option value="smartphones">Celulares</option>
+          <option value="mobile-accessories">Accesorios</option>
+        </select>
+      </div>
+    </section>
+  );
+}
