5f110c4f95f3f1e6f491e378ff727437944bfae9
AdrianGerman <adrian.german1019@gmail.com>
2024-06-24 09:40:09 -0600

    fix

--- projects/06-shopping-cart/src/App.jsx
+++ projects/06-shopping-cart/src/App.jsx
@@ -1,20 +1,18 @@
 import { products as initialProducts } from "./mocks/products.json";
 import { Products } from "./components/Products";
-import { useState } from "react";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
 import { useFilters } from "./hooks/useFilters";
 
 function App() {
-  const [products] = useState(initialProducts);
-  const { filters, filterProducts } = useFilters();
-  const filteredProducts = filterProducts(products);
+  const { filterProducts } = useFilters();
+  const filteredProducts = filterProducts(initialProducts);
 
   return (
     <>
       <Header />
       <Products products={filteredProducts} />
-      <Footer filters={filters} />
+      <Footer />
     </>
   );
 }
