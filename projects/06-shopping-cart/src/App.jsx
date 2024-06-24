182f80c42a2cb1748c929ecf9aa8c4f9455a1d7e
AdrianGerman <adrian.german1019@gmail.com>
2024-06-24 09:07:36 -0600

    create context in React

--- projects/06-shopping-cart/src/App.jsx
+++ projects/06-shopping-cart/src/App.jsx
@@ -1,11 +1,12 @@
 import { products as initialProducts } from "./mocks/products.json";
 import { Products } from "./components/Products";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
+import { FiltersContext } from "./context/filters";
 
 function useFilters() {
-  const [filters, setFilters] = useState({ category: "all", minPrice: 0 });
+  const { filters, setFilters } = useContext(FiltersContext);
 
   const filterProducts = (products) => {
     return products.filter((product) => {
@@ -15,19 +16,19 @@ function useFilters() {
       );
     });
   };
-  return { filterProducts, setFilters };
+  return { filters, filterProducts, setFilters };
 }
 
 function App() {
   const [products] = useState(initialProducts);
-  const { filterProducts, setFilters } = useFilters();
+  const { filters, filterProducts, setFilters } = useFilters();
   const filteredProducts = filterProducts(products);
 
   return (
     <>
       <Header changeFilters={setFilters} />
       <Products products={filteredProducts} />
-      <Footer />
+      <Footer filters={filters} />
     </>
   );
 }
