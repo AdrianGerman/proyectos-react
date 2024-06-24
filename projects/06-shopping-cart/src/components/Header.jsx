5f744bf4dd41ab5e94f375b6119c51cf9521fbb3
AdrianGerman <adrian.german1019@gmail.com>
2024-06-24 09:32:21 -0600

    avoid prop drilling with useContext

--- projects/06-shopping-cart/src/components/Header.jsx
+++ projects/06-shopping-cart/src/components/Header.jsx
@@ -1,11 +1,10 @@
-/* eslint-disable react/prop-types */
 import { Filters } from "./Filters";
 
-export function Header({ changeFilters }) {
+export function Header() {
   return (
     <header>
       <h1>Shopping cart 🛒</h1>
-      <Filters onChange={changeFilters} />
+      <Filters />
     </header>
   );
 }
