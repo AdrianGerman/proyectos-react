5cd0ff4bae68295a226f8c6927dca32ee15d2f08
AdrianGerman <adrian.german1019@gmail.com>
2024-06-25 07:44:59 -0600

    shopping cart creation

---
+++ projects/06-shopping-cart/src/components/Cart.jsx
@@ -0,0 +1,38 @@
+import { CartIcon, ClearCartIcon } from "./Icons";
+import { useId } from "react";
+import "./Cart.css";
+
+export function Cart() {
+  const cartCheckboxId = useId();
+  return (
+    <>
+      <label className="cart-button" htmlFor={cartCheckboxId}>
+        <CartIcon />
+      </label>
+      <input id={cartCheckboxId} type="checkbox" hidden />
+
+      <aside className="cart">
+        <ul>
+          <li>
+            <img
+              src="https://cdn.dummyjson.com/products/images/smartphones/iPhone%205s/thumbnail.png"
+              alt="iPhone"
+            />
+            <div>
+              <strong>iPhone</strong> - $1499
+            </div>
+
+            <footer>
+              <small>Qty: 1</small>
+              <button>+</button>
+            </footer>
+          </li>
+        </ul>
+
+        <button>
+          <ClearCartIcon />
+        </button>
+      </aside>
+    </>
+  );
+}
