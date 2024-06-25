d23f4eefba6d25333e6dc0b3c7771d5f6012e5df
AdrianGerman <adrian.german1019@gmail.com>
2024-06-25 10:09:30 -0600

    show cart info

--- projects/06-shopping-cart/src/components/Cart.jsx
+++ projects/06-shopping-cart/src/components/Cart.jsx
@@ -1,9 +1,28 @@
+/* eslint-disable react/prop-types */
 import { CartIcon, ClearCartIcon } from "./Icons";
 import { useId } from "react";
 import "./Cart.css";
+import { useCart } from "../hooks/useCart";
+
+function CartItem({ thumbnail, price, title, quantity, addToCart }) {
+  return (
+    <li>
+      <img src={thumbnail} alt={title} />
+      <div>
+        <strong>{title}</strong> - ${price}
+      </div>
+
+      <footer>
+        <small>Qty: {quantity}</small>
+        <button onClick={addToCart}>+</button>
+      </footer>
+    </li>
+  );
+}
 
 export function Cart() {
   const cartCheckboxId = useId();
+  const { cart, clearCart, addToCart } = useCart();
   return (
     <>
       <label className="cart-button" htmlFor={cartCheckboxId}>
@@ -13,23 +32,12 @@ export function Cart() {
 
       <aside className="cart">
         <ul>
-          <li>
-            <img
-              src="https://cdn.dummyjson.com/products/images/smartphones/iPhone%205s/thumbnail.png"
-              alt="iPhone"
-            />
-            <div>
-              <strong>iPhone</strong> - $1499
-            </div>
-
-            <footer>
-              <small>Qty: 1</small>
-              <button>+</button>
-            </footer>
-          </li>
+          {cart.map((product) => (
+            <CartItem key={product.id} addToCart={() => addToCart(product)} {...product} />
+          ))}
         </ul>
 
-        <button>
+        <button onClick={clearCart}>
           <ClearCartIcon />
         </button>
       </aside>
