b76573ab8ec61bbfee905317493ad1534532bbd1
AdrianGerman <adrian.german1019@gmail.com>
2024-06-27 08:14:03 -0600

    basic structure of the application

--- projects/07-heim-router/src/App.jsx
+++ projects/07-heim-router/src/App.jsx
@@ -1,9 +1,36 @@
 import "./App.css";
 
+function HomePage() {
+  return (
+    <>
+      <h1>Home</h1>
+      <p>Esta es una página de ejemplo para crear un React Router desde cero</p>
+      <a href="/about">Ir a Sobre Nosotros</a>
+    </>
+  );
+}
+
+function AboutPage() {
+  return (
+    <>
+      <div>
+        <img
+          src="https://pbs.twimg.com/profile_images/1776312145816227840/ISlE225__400x400.jpg"
+          alt="foto_de_perfil"
+        />
+        <h1>About</h1>
+        <p>Hola! Me llamo Adrian German y estoy creando un clon de React Router.</p>
+        <a href="/">Ir a la home</a>
+      </div>
+    </>
+  );
+}
+
 function App() {
   return (
     <>
-      <h1>Heim Router</h1>
+      <HomePage />
+      <AboutPage />
     </>
   );
 }
