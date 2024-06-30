bc130c62e95da7d9e5976e4b79648e7e768e8e61
AdrianGerman <adrian.german1019@gmail.com>
2024-06-30 08:42:09 -0600

    more tests and corrections found by the tests

--- projects/07-heim-router/src/Router.jsx
+++ projects/07-heim-router/src/Router.jsx
@@ -2,17 +2,18 @@
 import { useState, useEffect, Children } from "react";
 import { EVENTS } from "./const";
 import { match } from "path-to-regexp";
+import { getCurrentPath } from "./utils";
 
 export function Router({
   children,
   routes = [],
   defaultComponent: DefaultComponent = () => <h1>404 error!</h1>
 }) {
-  const [currentPath, setCurrentPath] = useState(window.location.pathname);
+  const [currentPath, setCurrentPath] = useState(getCurrentPath());
 
   useEffect(() => {
     const onLocationChange = () => {
-      setCurrentPath(window.location.pathname);
+      setCurrentPath(getCurrentPath());
     };
     window.addEventListener(EVENTS.PUSHSTATE, onLocationChange);
     window.addEventListener(EVENTS.POPSTATE, onLocationChange);
@@ -32,7 +33,7 @@ export function Router({
     return isRoute ? props : null;
   });
 
-  const routesToUse = routes.concat(routesFromChildren);
+  const routesToUse = routes.concat(routesFromChildren).filter(Boolean);
 
   const Page = routesToUse.find(({ path }) => {
     if (path === currentPath) return true;
