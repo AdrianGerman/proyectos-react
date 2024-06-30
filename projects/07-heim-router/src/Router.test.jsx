ca2a5f886b17c080218462e51df13215e73df1de
AdrianGerman <adrian.german1019@gmail.com>
2024-06-30 09:28:48 -0600

    finish testing our Router package

--- projects/07-heim-router/src/Router.test.jsx
+++ projects/07-heim-router/src/Router.test.jsx
@@ -1,6 +1,8 @@
 import { describe, it, expect, beforeEach, vi } from "vitest";
-import { render, screen, cleanup } from "@testing-library/react";
+import { render, screen, cleanup, fireEvent } from "@testing-library/react";
 import { Router } from "./Router";
+import { Route } from "./Route";
+import { Link } from "./Link";
 import { getCurrentPath } from "./utils";
 
 vi.mock("./utils.js", () => ({
@@ -36,5 +38,33 @@ describe("Router", () => {
     ];
     render(<Router routes={routes} />);
     expect(screen.getByText("About")).toBeTruthy();
+  });
+
+  it("should navigate using Links", async () => {
+    getCurrentPath.mockReturnValueOnce("/");
+    render(
+      <Router>
+        <Route
+          path="/"
+          Component={() => {
+            return (
+              <>
+                <h1>Home</h1>
+                <Link to="/about">Go to About</Link>
+              </>
+            );
+          }}
+        />
+        <Route path="/about" Component={() => <h1>About</h1>} />
+      </Router>
+    );
+    // Click on the link
+    const anchor = screen.getByText(/Go to About/);
+    fireEvent.click(anchor);
+
+    const aboutTitle = await screen.findByText("About");
+
+    // Check that the new route is rendered
+    expect(aboutTitle).toBeTruthy();
   });
 });
