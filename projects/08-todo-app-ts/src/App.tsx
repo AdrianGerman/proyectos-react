9bb441faaf94b89bdc05668150e60106efbd9832
AdrianGerman <adrian.german1019@gmail.com>
2024-07-01 08:37:19 -0600

    rendering task list

--- projects/08-todo-app-ts/src/App.tsx
+++ projects/08-todo-app-ts/src/App.tsx
@@ -1,9 +1,30 @@
+import { useState } from "react";
 import "./App.css";
+import { Todos } from "./components/Todos";
+
+const mockTodos = [
+  {
+    id: "1",
+    title: "todo 1",
+    completed: false
+  },
+  {
+    id: "2",
+    title: "todo 2",
+    completed: false
+  },
+  {
+    id: "3",
+    title: "todo 3",
+    completed: false
+  }
+];
 
 const App = (): JSX.Element => {
+  const [todos] = useState(mockTodos);
   return (
     <>
-      <h1>Todo MVC</h1>
+      <Todos todos={todos} />
     </>
   );
 };
