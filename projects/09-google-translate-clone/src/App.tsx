5c58cfde2cad1400dced94edc2ecc87ef020c02f
AdrianGerman <adrian.german1019@gmail.com>
2024-07-09 09:08:41 -0600

    applying the steps of the reducer

--- projects/09-google-translate-clone/src/App.tsx
+++ projects/09-google-translate-clone/src/App.tsx
@@ -1,7 +1,62 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import { useReducer } from "react";
 import "./App.css";
 
+const initialState = {
+  fromLanguage: "auto",
+  toLanguage: "en",
+  formText: "",
+  result: "",
+  loading: false
+};
+
+function reducer(state, action) {
+  const { type, payload } = action;
+
+  if (type === "INTERCHANGE_LANGUAGES") {
+    return {
+      ...state,
+      fromLanguage: state.toLanguage,
+      toLanguage: state.fromLanguage
+    };
+  }
+
+  if (type === "SET_FROM_LANGUAGES") {
+    return {
+      ...state,
+      fromLanguage: payload
+    };
+  }
+
+  if (type === "SET_TO_LANGUAGES") {
+    return {
+      ...state,
+      toLanguages: payload
+    };
+  }
+
+  if (type === "SET_FROM_TEXT") {
+    return {
+      ...state,
+      loading: true,
+      fromText: payload,
+      result: ""
+    };
+  }
+
+  if (type === "SET_RESULT") {
+    return {
+      ...state,
+      loading: false,
+      result: payload
+    };
+  }
+
+  return state;
+}
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
   return (
     <>
       <div>
