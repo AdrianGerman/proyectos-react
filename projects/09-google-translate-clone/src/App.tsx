25cad55d8beefb2d27de398656f5cba11780f5af
AdrianGerman <adrian.german1019@gmail.com>
2024-07-09 12:23:37 -0600

    creating a custom hooks

--- projects/09-google-translate-clone/src/App.tsx
+++ projects/09-google-translate-clone/src/App.tsx
@@ -1,76 +1,16 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { useReducer } from "react";
 import "./App.css";
-import { Action, type State } from "./types.d";
-
-const initialState: State = {
-  fromLanguage: "auto",
-  toLanguage: "en",
-  fromText: "",
-  result: "",
-  loading: false
-};
-
-function reducer(state: State, action: Action) {
-  const { type } = action;
-
-  if (type === "INTERCHANGE_LANGUAGES") {
-    return {
-      ...state,
-      fromLanguage: state.toLanguage,
-      toLanguage: state.fromLanguage
-    };
-  }
-
-  if (type === "SET_FROM_LANGUAGES") {
-    return {
-      ...state,
-      fromLanguage: action.payload
-    };
-  }
-
-  if (type === "SET_TO_LANGUAGES") {
-    return {
-      ...state,
-      toLanguages: action.payload
-    };
-  }
-
-  if (type === "SET_FROM_TEXT") {
-    return {
-      ...state,
-      loading: true,
-      fromText: action.payload,
-      result: ""
-    };
-  }
-
-  if (type === "SET_RESULT") {
-    return {
-      ...state,
-      loading: false,
-      result: action.payload
-    };
-  }
-
-  return state;
-}
+import { useStore } from "./hooks/useStore";
 
 function App() {
-  const [{ fromLanguage, toLanguage, fromText, result, loading }, dispatch] = useReducer(
-    reducer,
-    initialState
-  );
-
-  console.log({ fromLanguage });
-
+  const { setFromLanguage } = useStore();
   return (
     <>
       <div>
         <h1>Google translate</h1>
         <button
           onClick={() => {
-            dispatch({ type: "SET_FROM_LANGUAGES", payload: "es" });
+            setFromLanguage("es");
           }}
         >
           Cambiar loading
---
+++ projects/09-google-translate-clone/src/hooks/useStore.ts
@@ -0,0 +1,93 @@
+import { Action, type State } from "../types.d";
+import { useReducer } from "react";
+
+const initialState: State = {
+  fromLanguage: "auto",
+  toLanguage: "en",
+  fromText: "",
+  result: "",
+  loading: false
+};
+
+function reducer(state: State, action: Action) {
+  const { type } = action;
+
+  if (type === "INTERCHANGE_LANGUAGES") {
+    return {
+      ...state,
+      fromLanguage: state.toLanguage,
+      toLanguage: state.fromLanguage
+    };
+  }
+
+  if (type === "SET_FROM_LANGUAGES") {
+    return {
+      ...state,
+      fromLanguage: action.payload
+    };
+  }
+
+  if (type === "SET_TO_LANGUAGES") {
+    return {
+      ...state,
+      toLanguages: action.payload
+    };
+  }
+
+  if (type === "SET_FROM_TEXT") {
+    return {
+      ...state,
+      loading: true,
+      fromText: action.payload,
+      result: ""
+    };
+  }
+
+  if (type === "SET_RESULT") {
+    return {
+      ...state,
+      loading: false,
+      result: action.payload
+    };
+  }
+
+  return state;
+}
+
+export function useStore() {
+  const [{ fromLanguage, toLanguage, fromText, result, loading }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+
+  const interchangeLanguages = () => dispatch({ type: "INTERCHANGE_LANGUAGES" });
+
+  const setFromLanguage = (payload: string) => {
+    dispatch({ type: "SET_FROM_LANGUAGES", payload });
+  };
+
+  const setToLanguage = (payload: string) => {
+    dispatch({ type: "SET_TO_LANGUAGES", payload });
+  };
+
+  const setFromText = (payload: string) => {
+    dispatch({ type: "SET_FROM_TEXT", payload });
+  };
+
+  const setResult = (payload: string) => {
+    dispatch({ type: "SET_RESULT", payload });
+  };
+
+  return {
+    fromLanguage,
+    toLanguage,
+    fromText,
+    result,
+    loading,
+    interchangeLanguages,
+    setFromLanguage,
+    setToLanguage,
+    setFromText,
+    setResult
+  };
+}
