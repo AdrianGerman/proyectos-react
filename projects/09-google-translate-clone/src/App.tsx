d85fb83ee3a5bb2790723617e2470a02b7949744
AdrianGerman <adrian.german1019@gmail.com>
2024-07-11 09:17:35 -0600

    textarea creation

--- projects/09-google-translate-clone/src/App.tsx
+++ projects/09-google-translate-clone/src/App.tsx
@@ -1,28 +1,42 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import { useStore } from "./hooks/useStore";
-import { Container, Row, Col, Button } from "react-bootstrap";
+import { Container, Row, Col, Button, Form, Stack } from "react-bootstrap";
 import { AUTO_LANGUAGE } from "./constants";
 import { ArrowsIcon } from "./components/Icons";
 import { LanguageSelector } from "./components/LanguageSelector";
 import { SectionType } from "./types.d";
+import { TextArea } from "./components/TextArea";
 
 function App() {
-  const { fromLanguage, toLanguage, interchangeLanguages, setFromLanguage, setToLanguage } =
-    useStore();
+  const {
+    loading,
+    fromLanguage,
+    toLanguage,
+    fromText,
+    result,
+    interchangeLanguages,
+    setFromLanguage,
+    setToLanguage,
+    setFromText,
+    setResult
+  } = useStore();
   return (
     <Container fluid>
-      <h1>Google translate</h1>
+      <h2>Google translate</h2>
       <Row>
         <Col>
-          <LanguageSelector
-            type={SectionType.From}
-            value={fromLanguage}
-            onChange={setFromLanguage}
-          />
-          {fromLanguage}
+          <Stack gap={2}>
+            <LanguageSelector
+              type={SectionType.From}
+              value={fromLanguage}
+              onChange={setFromLanguage}
+            />
+            <TextArea type={SectionType.From} value={fromText} onChange={setFromText} />
+          </Stack>
         </Col>
-        <Col>
+
+        <Col xs="auto">
           <Button
             variant="link"
             disabled={fromLanguage === AUTO_LANGUAGE}
@@ -31,9 +45,12 @@ function App() {
             <ArrowsIcon />
           </Button>
         </Col>
+
         <Col>
-          <LanguageSelector type={SectionType.To} value={toLanguage} onChange={setToLanguage} />
-          {toLanguage}
+          <Stack gap={2}>
+            <LanguageSelector type={SectionType.To} value={toLanguage} onChange={setToLanguage} />
+            <TextArea loading={loading} type={SectionType.To} value={result} onChange={setResult} />
+          </Stack>
         </Col>
       </Row>
     </Container>
---
+++ projects/09-google-translate-clone/src/components/TextArea.tsx
@@ -0,0 +1,41 @@
+import { Form } from "react-bootstrap";
+import { SectionType } from "../types.d";
+import "./TextArea.css";
+
+type Props = {
+  type: SectionType;
+  loading?: boolean;
+  onChange: (value: string) => void;
+  value: string;
+};
+
+const commondStyles = { height: "200px" };
+
+const getPlaceholder = ({ type, loading }: { type: SectionType; loading?: boolean }) => {
+  if (type === SectionType.From) return "Introducir texto";
+  if (loading === true) return "Cargando...";
+  return "Traducción";
+};
+
+export const TextArea = ({ type, loading, value, onChange }: Props) => {
+  const styles =
+    type === SectionType.From
+      ? commondStyles
+      : { ...commondStyles, backgroundColor: "#303030", border: 0 };
+
+  const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    onChange(event.target.value);
+  };
+
+  return (
+    <Form.Control
+      autoFocus={type === SectionType.From}
+      as="textarea"
+      placeholder={getPlaceholder({ type, loading })}
+      value={value}
+      onChange={handleChange}
+      className="textarea"
+      style={styles}
+    />
+  );
+};
