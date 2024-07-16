ee8efd66967ab8fda845ec832a79e279a136c39b
AdrianGerman <adrian.german1019@gmail.com>
2024-07-16 07:52:16 -0600

    error handling and local state

--- projects/10-crud-redux/src/components/CreateNewUser.tsx
+++ projects/10-crud-redux/src/components/CreateNewUser.tsx
@@ -1,9 +1,10 @@
-import { Button, Card, TextInput, Title } from "@tremor/react"
+import { Badge, Button, Card, TextInput, Title } from "@tremor/react"
 import { useUserActions } from "../hooks/useUserActions"
-import React from "react"
+import { useState } from "react"
 
 export function CreateNewUser() {
   const { addUser } = useUserActions()
+  const [result, setResult] = useState<"ok" | "ko" | null>(null)
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
@@ -14,7 +15,13 @@ export function CreateNewUser() {
     const email = formData.get("email") as string
     const github = formData.get("github") as string
 
+    if (!name || !email || !github) {
+      return setResult("ko")
+    }
+
     addUser({ name, email, github })
+    setResult("ok")
+    form.reset()
   }
 
   return (
@@ -27,6 +34,8 @@ export function CreateNewUser() {
 
         <div>
           <Button type="submit">Create</Button>
+          <span>{result === "ok" && <Badge color="green">Guardado correctamente</Badge>}</span>
+          <span>{result === "ko" && <Badge color="red">Error con los campos</Badge>}</span>
         </div>
       </form>
     </Card>
