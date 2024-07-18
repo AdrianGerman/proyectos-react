9fe693ea575fcf59032f438114c990288cc8e490
AdrianGerman <adrian.german1019@gmail.com>
2024-07-18 12:11:17 -0600

    data order by country

--- projects/11-typescript-prueba-tecnica/src/App.tsx
+++ projects/11-typescript-prueba-tecnica/src/App.tsx
@@ -6,10 +6,16 @@ import { UserList } from "./components/UsersList"
 function App() {
   const [users, setUsers] = useState<User[]>([])
   const [showColors, setShowColors] = useState(false)
+  const [sortByCountry, setSortByCountry] = useState(false)
 
   const toggleColors = () => {
     setShowColors(!showColors)
   }
+
+  const toggleSortByCount = () => {
+    setSortByCountry((prevState) => !prevState)
+  }
+
   useEffect(() => {
     fetch("https://randomuser.me/api?results=100")
       .then(async (res) => await res.json())
@@ -21,15 +27,24 @@ function App() {
       })
   }, [])
 
+  const sortedUsers = sortByCountry
+    ? [...users].sort((a, b) => {
+        return a.location.country.localeCompare(b.location.country)
+      })
+    : users
+
   return (
     <>
       <div>
         <h1>Prueba técnica</h1>
         <header>
           <button onClick={toggleColors}>Colorear filas</button>
+          <button onClick={toggleSortByCount}>
+            {sortByCountry ? "No ordenar por país" : "Ordenar por país"}
+          </button>
         </header>
         <main>
-          <UserList showColors={showColors} users={users} />
+          <UserList showColors={showColors} users={sortedUsers} />
         </main>
       </div>
     </>
