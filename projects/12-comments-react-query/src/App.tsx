6ceb39ab4022d64b779a2f2fdf5424a5c29fb6d5
AdrianGerman <adrian.german1019@gmail.com>
2024-07-25 10:33:44 -0600

    form component and eslint rule

--- projects/12-comments-react-query/src/App.tsx
+++ projects/12-comments-react-query/src/App.tsx
@@ -1,11 +1,26 @@
+import "./App.css"
+
+import { FormInput, FormTextArea } from "./components/Form"
 
 function App() {
-
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <h1 className="text-3xl font-bold underline">React query</h1>
+      <main>
+        <div className="col-span-1 bg-white p-8">{/* aqui se van a mostrar los comentarios */}</div>
+        <div className="col-span-1 bg-black p-8">
+          <form className="max-w-xl m-auto block px-4">
+            <FormInput />
+            <FormTextArea />
+            <button
+              type="submit"
+              className="mt-4 px-12 text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm py-2.5 text-center mr-2 mb-2"
+            >
+              Enviar comentario
+            </button>
+          </form>
+        </div>
+      </main>
     </>
   )
 }
