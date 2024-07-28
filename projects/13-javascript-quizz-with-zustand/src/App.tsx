16b694094f98fb24855ff6923269a7adea17945d
AdrianGerman <adrian.german1019@gmail.com>
2024-07-28 07:43:38 -0600

    install zustand, type with typescript and global status

--- projects/13-javascript-quizz-with-zustand/src/App.tsx
+++ projects/13-javascript-quizz-with-zustand/src/App.tsx
@@ -2,8 +2,12 @@ import { Container, Stack, Typography } from "@mui/material"
 import { JavaScriptLogo } from "./JavaScriptLogo"
 import "./App.css"
 import { Start } from "./Start"
+import { useQuestionsStore } from "./store/question"
 
 function App() {
+  const questions = useQuestionsStore((state) => state.questions)
+  console.log(questions)
+
   return (
     <>
       <Container maxWidth="sm">
@@ -14,7 +18,8 @@ function App() {
           </Typography>
         </Stack>
 
-        <Start />
+        {questions.length === 0 && <Start />}
+        {questions.length > 0 && <h4>El juego a comenzado</h4>}
       </Container>
     </>
   )
---
+++ projects/13-javascript-quizz-with-zustand/src/store/question.ts
@@ -0,0 +1,29 @@
+import { create } from "zustand"
+import { Question } from "../types.d"
+
+interface State {
+  questions: Question[]
+  currentQuestion: number
+  fetchQuestions: (limit: number) => Promise<void>
+}
+
+export const useQuestionsStore = create<State>((set, get) => {
+  return {
+    questions: [],
+    currentQuestion: 0,
+
+    fetchQuestions: async (limit: number) => {
+      set({
+        questions: [
+          {
+            id: 1,
+            question: "¿Cuál es la salida de este código?",
+            code: "console.log(typeof NaN)",
+            answers: ["undefined", "NaN", "string", "number"],
+            correctAnswer: 3
+          }
+        ]
+      })
+    }
+  }
+})
