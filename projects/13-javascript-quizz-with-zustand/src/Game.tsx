5ea8d1fdc9cc03587f0b79fee4915157c1969b31
AdrianGerman <adrian.german1019@gmail.com>
2024-07-29 07:12:53 -0600

    adding syntax highlight

--- projects/13-javascript-quizz-with-zustand/src/Game.tsx
+++ projects/13-javascript-quizz-with-zustand/src/Game.tsx
@@ -2,11 +2,16 @@
 import { Card, Typography } from "@mui/material"
 import { useQuestionsStore } from "./store/question"
 import { type Question as QuestionType } from "./types.d"
+import SyntaxHighlighter from "react-syntax-highlighter"
+import { gradientDark } from "react-syntax-highlighter/dist/esm/styles/hljs"
 
 const Question = ({ info }: { info: QuestionType }) => {
   return (
-    <Card variant="outlined">
+    <Card variant="outlined" sx={{ textAlign: "left" }}>
       <Typography variant="h5">{info.question}</Typography>
+      <SyntaxHighlighter language="javascript" style={gradientDark}>
+        {info.code}
+      </SyntaxHighlighter>
     </Card>
   )
 }
