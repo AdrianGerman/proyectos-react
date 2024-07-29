a631fc56effffca6369029a0903133b8e7893648
AdrianGerman <adrian.german1019@gmail.com>
2024-07-29 07:34:51 -0600

    adding Lists of answers

--- projects/13-javascript-quizz-with-zustand/src/Game.tsx
+++ projects/13-javascript-quizz-with-zustand/src/Game.tsx
@@ -1,5 +1,5 @@
 // import { IconButton, Stack } from "@mui/material"
-import { Card, Typography } from "@mui/material"
+import { Card, List, ListItem, ListItemButton, ListItemText, Typography } from "@mui/material"
 import { useQuestionsStore } from "./store/question"
 import { type Question as QuestionType } from "./types.d"
 import SyntaxHighlighter from "react-syntax-highlighter"
@@ -7,11 +7,21 @@ import { gradientDark } from "react-syntax-highlighter/dist/esm/styles/hljs"
 
 const Question = ({ info }: { info: QuestionType }) => {
   return (
-    <Card variant="outlined" sx={{ textAlign: "left" }}>
+    <Card variant="outlined" sx={{ bgcolor: "#222", p: 2, textAlign: "left", marginTop: 4 }}>
       <Typography variant="h5">{info.question}</Typography>
       <SyntaxHighlighter language="javascript" style={gradientDark}>
         {info.code}
       </SyntaxHighlighter>
+
+      <List sx={{ bgcolor: "#333" }} disablePadding>
+        {info.answers.map((answer, index) => (
+          <ListItem key={index} disablePadding divider>
+            <ListItemButton>
+              <ListItemText primary={answer} sx={{ textAlign: "center" }} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
     </Card>
   )
 }
