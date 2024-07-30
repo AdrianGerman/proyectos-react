cf6527e0556282d27d197c47497b1a287cb3f064
AdrianGerman <adrian.german1019@gmail.com>
2024-07-30 09:46:28 -0600

    data persistence with LocalStorage and reset button

--- projects/13-javascript-quizz-with-zustand/src/Footer.tsx
+++ projects/13-javascript-quizz-with-zustand/src/Footer.tsx
@@ -1,11 +1,17 @@
+import { Button } from "@mui/material"
 import { useQuestionData } from "./hooks/useQuestionsData"
+import { useQuestionsStore } from "./store/question"
 
 export const Footer = () => {
   const { correct, incorrect, unanswered } = useQuestionData()
+  const reset = useQuestionsStore((state) => state.reset)
 
   return (
     <footer style={{ marginTop: "16px" }}>
       <strong>{`✅ ${correct} Correctas - ❌ ${incorrect} Incorrectas - ❓ ${unanswered} Sin responder`}</strong>
+      <div style={{ marginTop: "16px" }}>
+        <Button onClick={() => reset()}>Resetear juego</Button>
+      </div>
     </footer>
   )
 }
--- projects/13-javascript-quizz-with-zustand/src/store/question.ts
+++ projects/13-javascript-quizz-with-zustand/src/store/question.ts
@@ -1,6 +1,7 @@
 import { create } from "zustand"
 import { Question } from "../types.d"
 import confetti from "canvas-confetti"
+import { persist } from "zustand/middleware"
 
 interface State {
   questions: Question[]
@@ -9,60 +10,70 @@ interface State {
   selectAnswer: (questionId: number, answerIndex: number) => void
   goNextQuestion: () => void
   goPreviousQuestion: () => void
+  reset: () => void
 }
 
-export const useQuestionsStore = create<State>((set, get) => {
-  return {
-    questions: [],
-    currentQuestion: 0,
+export const useQuestionsStore = create<State>()(
+  persist(
+    (set, get) => {
+      return {
+        questions: [],
+        currentQuestion: 0,
 
-    fetchQuestions: async (limit: number) => {
-      const res = await fetch("http://localhost:5173/data.json")
-      const json = await res.json()
-      console.log(json)
+        fetchQuestions: async (limit: number) => {
+          const res = await fetch("http://localhost:5173/data.json")
+          const json = await res.json()
+          console.log(json)
 
-      const questions = json.sort(() => Math.random() - 0.5).slice(0, limit)
-      set({ questions })
-    },
+          const questions = json.sort(() => Math.random() - 0.5).slice(0, limit)
+          set({ questions })
+        },
 
-    selectAnswer: (questionId: number, answerIndex: number) => {
-      const { questions } = get()
-      // usar el structureClone para clonar el objeto
-      const newQuestions = structuredClone(questions)
-      // encontramos el índice de la pregunta
-      const questionIndex = newQuestions.findIndex((q) => q.id === questionId)
-      // obtenemos la información de la pregunta
-      const questionInfo = newQuestions[questionIndex]
-      // vemos si el usuario a seleccionado la respuesta correcta
-      const isCorrectUserAnswer = questionInfo.correctAnswer === answerIndex
-      // si la respuesta es correcta, lanzamos confetti
-      if (isCorrectUserAnswer) confetti()
-      // cambiar esta información en la copia de la pregunta
-      newQuestions[questionIndex] = {
-        ...questionInfo,
-        isCorrectUserAnswer,
-        userSelectedAnswer: answerIndex
+        selectAnswer: (questionId: number, answerIndex: number) => {
+          const { questions } = get()
+          // usar el structureClone para clonar el objeto
+          const newQuestions = structuredClone(questions)
+          // encontramos el índice de la pregunta
+          const questionIndex = newQuestions.findIndex((q) => q.id === questionId)
+          // obtenemos la información de la pregunta
+          const questionInfo = newQuestions[questionIndex]
+          // vemos si el usuario a seleccionado la respuesta correcta
+          const isCorrectUserAnswer = questionInfo.correctAnswer === answerIndex
+          // si la respuesta es correcta, lanzamos confetti
+          if (isCorrectUserAnswer) confetti()
+          // cambiar esta información en la copia de la pregunta
+          newQuestions[questionIndex] = {
+            ...questionInfo,
+            isCorrectUserAnswer,
+            userSelectedAnswer: answerIndex
+          }
+          // actualizamos el estado
+          set({ questions: newQuestions })
+        },
+
+        goNextQuestion: () => {
+          const { currentQuestion, questions } = get()
+          const nextQuestion = currentQuestion + 1
+
+          if (nextQuestion < questions.length) {
+            set({ currentQuestion: nextQuestion })
+          }
+        },
+
+        goPreviousQuestion: () => {
+          const { currentQuestion } = get()
+          const previousQuestion = currentQuestion - 1
+
+          if (previousQuestion >= 0) {
+            set({ currentQuestion: previousQuestion })
+          }
+        },
+
+        reset: () => {
+          set({ currentQuestion: 0, questions: [] })
+        }
       }
-      // actualizamos el estado
-      set({ questions: newQuestions })
     },
-
-    goNextQuestion: () => {
-      const { currentQuestion, questions } = get()
-      const nextQuestion = currentQuestion + 1
-
-      if (nextQuestion < questions.length) {
-        set({ currentQuestion: nextQuestion })
-      }
-    },
-
-    goPreviousQuestion: () => {
-      const { currentQuestion } = get()
-      const previousQuestion = currentQuestion - 1
-
-      if (previousQuestion >= 0) {
-        set({ currentQuestion: previousQuestion })
-      }
-    }
-  }
-})
+    { name: "questions" }
+  )
+)
